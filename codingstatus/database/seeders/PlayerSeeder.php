99ec00cd63d3999b9b8be85ec8ee5ab14cdbb143
LukaMijovic <mijovic.luka@gmail.com>
2022-12-19 17:36:03 +0100

    Kreiran seeder

--- codingstatus/database/seeders/PlayerSeeder.php
+++ codingstatus/database/seeders/PlayerSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Player;
+use Illuminate\Support\Str;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Ramsey\Uuid\Type\Integer;
 
 class PlayerSeeder extends Seeder
 {
@@ -14,6 +18,43 @@ class PlayerSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Player::truncate();
+
+        // $player = Player::factory()->create();
+
+        // $p1 = Player::create([
+        //     "firstname" => "Mohamed",
+        //     "lastname" => "Salah",
+        //     "shirtNumber" => 11,
+        //     "age" => 29,
+        //     "nationality" => "Egipcanin",
+        //     "league" => "Premijer liga"
+        // ]);
+        // $p2 = Player::create([
+        //     "firstname" => "Timo",
+        //     "lastname" => "Werner",
+        //     "shirtNumber" => 11,
+        //     "age" => 24,
+        //     "nationality" => "Nemac",
+        //     "league" => "Bundes liga"
+        // ]);
+        // $p3 = Player::create([
+        //     "firstname" => "Dusan",
+        //     "lastname" => "Vlahovic",
+        //     "shirtNumber" => 9,
+        //     "age" => 22,
+        //     "nationality" => "Srbin",
+        //     "league" => "Seria a"
+        // ]);
+
+        for ($i = random_int(1,5); $i < 5; $i++){
+            DB::table('players')->insert([
+                'firstname' => Str::random(10),
+                'lastname' => Str::random(10),
+                'shirtNumber' => random_int(1, 99),
+                'age' => random_int(18, 35),
+                'league'=> Str::random(10),
+            ]);
+        }
     }
 }
