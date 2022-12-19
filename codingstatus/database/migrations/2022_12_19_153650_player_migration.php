1127e64f57fad037aadd58a946c4117e20a12085
LukaMijovic <mijovic.luka@gmail.com>
2022-12-19 16:44:40 +0100

    Prva migracija

---
+++ codingstatus/database/migrations/2022_12_19_153650_player_migration.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('player', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->integer('shirtNumber', false, false);
+            $table->string('league');
+            $table->string('nationality');
+            $table->integer('age', false, false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('player');
+    }
+};
